9e15e561918557cebafccda89ebb849793445cb7
ngenziwingenzi <nshutihubert4@gmail.com>
2026-02-11 16:29:27 +0200

    add rendering lists exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 function App(){
+  const users = [
+    { id: 1, name: 'Nathan', role: 'Web Developer' },
+    { id: 2, name: 'John', role: 'Web Designer' },
+    { id: 3, name: 'Jane', role: 'Team Leader' },
+  ]
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <h2>Learning to code with React</h2>
-    </div>
+    <>
+     <h1>This is my List: </h1>
+     <ul>
+      {users.map(user => {
+        return (
+          <li key={user.id}>{user.name} serving as {user.role}</li>
+        )
+      }
+      )}
+     </ul>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementByID('root').render(
-  <StrictMode>
-    < App/>
-  </StrictMode>
-))
+// createRoot(document.getElementByID('root').render(
+//   <StrictMode>
+//     < App/>
+//   </StrictMode>
+// ))
 
 //Alternative(Less Performance)
 /* 
@@ -21,6 +21,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 )
 
 */
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App/>
+  </StrictMode>
+)
 
 
 
@@ -51,12 +56,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 
 
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
