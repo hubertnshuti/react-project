f94c4113ad8835ec8efcab4593fe05e4359a50ca
ngenziwingenzi <nshutihubert4@gmail.com>
2026-02-12 17:47:34 +0200

    add three components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
-function App(){
-  const users = [
-    { id: 1, name: 'Nathan', role: 'Web Developer' },
-    { id: 2, name: 'John', role: 'Web Designer' },
-    { id: 3, name: 'Jane', role: 'Team Leader' },
-  ]
+// function App(){
+//   const users = [
+//     { id: 1, name: 'Nathan', role: 'Web Developer' },
+//     { id: 2, name: 'John', role: 'Web Designer' },
+//     { id: 3, name: 'Jane', role: 'Team Leader' },
+//   ]
 
+//   return (
+//     <>
+//      <h1>This is my List: </h1>
+//      <ul>
+//       {users.map(user => {
+//         return (
+//           <li key={user.id}>{user.name} serving as {user.role}</li>
+//         )
+//       }
+//       )}
+//      </ul>
+//     </>
+//   )
+// }
+
+// export default App
+
+export default function ParentComponent() {
   return (
     <>
-     <h1>This is my List: </h1>
-     <ul>
-      {users.map(user => {
-        return (
-          <li key={user.id}>{user.name} serving as {user.role}</li>
-        )
-      }
-      )}
-     </ul>
+      <UserComponent />
+      <ProfileComponent />
+      <FeedComponent />
     </>
-  )
+  );
 }
 
-export default App
+function UserComponent() {
+  return <h1> User Component </h1>;
+}
+
+function ProfileComponent() {
+  return <h1> Profile Component </h1>;
+}
+
+function FeedComponent() {
+  return <h1> Feed Component</h1>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ParentComponent from './App.jsx'
 
 // createRoot(document.getElementByID('root').render(
 //   <StrictMode>
@@ -23,7 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    <ParentComponent/>
   </StrictMode>
 )
 
