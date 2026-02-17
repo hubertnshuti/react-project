f8c51ecb75f947b7faa5473143cba4ccd021f5c1
ngenziwingenzi <nshutihubert4@gmail.com>
2026-02-17 18:13:33 +0200

    add large users component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 export default function ParentComponent() {
   return (
     <>
+      <mainUsersComponent/>
       <UserComponent 
       name = "John"
       age = {21}
@@ -10,6 +11,27 @@ export default function ParentComponent() {
       <FeedComponent />
     </>
   );
+}
+
+function mainUsersComponent(){
+  const users = [
+    { id: 1, name: 'Nathan', role: 'Web Developer' },
+    { id: 2, name: 'John', role: 'Web Designer' },
+    { id: 3, name: 'Jane', role: 'Team Leader' },
+  ]
+  return (
+    <>
+      <h1>This is my List: </h1>
+      <ul>
+        {users.map(user => {
+          return (
+            <li key={user.id}>{user.name} serving as {user.role}</li>
+          )
+        }
+        )}
+      </ul>
+    </>
+  )
 }
 
 function UserComponent(props) {
@@ -23,3 +45,5 @@ function ProfileComponent() {
 function FeedComponent() {
   return <h1> Feed Component</h1>;
 }
+
+
