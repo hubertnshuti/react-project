036b1b312b6782fd8cdd68b6fb99562cf40e5279
ngenziwingenzi <nshutihubert4@gmail.com>
2026-02-17 18:02:44 +0200

    fix component error

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-// function App(){
-//   const users = [
-//     { id: 1, name: 'Nathan', role: 'Web Developer' },
-//     { id: 2, name: 'John', role: 'Web Designer' },
-//     { id: 3, name: 'Jane', role: 'Team Leader' },
-//   ]
-
-//   return (
-//     <>
-//      <h1>This is my List: </h1>
-//      <ul>
-//       {users.map(user => {
-//         return (
-//           <li key={user.id}>{user.name} serving as {user.role}</li>
-//         )
-//       }
-//       )}
-//      </ul>
-//     </>
-//   )
-// }
-// export default App
-
 export default function ParentComponent() {
   return (
     <>
